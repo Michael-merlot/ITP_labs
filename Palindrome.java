0139d51392b1d63dbf6700c5df905db6cd56eea9
Michael-merlot <124175319+Michael-merlot@users.noreply.github.com>
2023-09-15 11:00:02 +0300

    Update new files

--- Palindrome.java
+++ Palindrome.java
@@ -12,14 +12,15 @@ public class Palindrome {
     // Массивы с 0 и кол-во элементов в массиве args
     // Внутри цикла каждый элемент сохраняется в s для обработки
     public static String reverseString(String s) {
-        String reversed = "";
+        String reversed = ""; // склеивание
         for (int i = s.length() - 1; i >= 0; i--) { // Переборка символов в строке s
             reversed += s.charAt(i); // Метод для извлечения символа из строки с индексом i
-        } // s в обратном порядке
+        } // charAt - извлекает символ у строки
         return reversed; // Принимает строку s и возвращает обратно
     }
 
     public static boolean isPalindrome(String s) {
         return s.equals(reverseString(s));
     }
-}
+} // java Palindrome madam racecar apple kayak song noon
+
--- src/Primes.java
+++ src/Primes.java
@@ -1,15 +1,15 @@
 public class Primes {
     public static void main(String[] args) {
         for (int i = 2; i <= 100; i++) {
-            if (isPrime(i)) {
+            if (isPrime(i)) { // Простой
                 System.out.print(i + " ");
             }
         }
     }
 
     public static boolean isPrime(int n) {
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
+        for (int i = 2; i <= Math.sqrt(n); i++) { // Оптимизация
+            if (n % i == 0) { // Проверка, если без то n не простое
                 return false;
             }
         }
